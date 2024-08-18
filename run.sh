7efa810dd4018b3413593c75870948a920711738
root <root@less-letter.yeezyhost.net>
2024-08-18 14:33:09 +0200

    docker autostart

--- run.sh
+++ run.sh
@@ -22,6 +22,7 @@ build_image() {
 create_and_run_container() {
     echo "Creating and starting container..."
     docker run -d --name photo_editor --env-file .env photo_editor
+    docker update --restart unless-stopped photo_editor
 }
 
 # Function to stop the container
